3f241226eaf418a83c5533a0a06fafb79ebad0a3
yurilealjaque <yurilealjaque2014@gmail.com>
2026-04-12 21:41:29 -0400

    v2 ejercicio

--- ejercicios para practicar funciones/convertidor_a_segundos.py
+++ ejercicios para practicar funciones/convertidor_a_segundos.py
@@ -1,6 +1,6 @@
 """Programa que convierta una unidad en dias, horas, minutos y segundos a segundos"""
 
-def calcularSegundos(dias, horas, minutos, segundos):
+"""def calcularSegundos(dias, horas, minutos, segundos):
     segundosTotales = 0
     segundosTotales += segundos
     segundosTotales += minutos * 60
@@ -15,4 +15,17 @@ segundos = int(input("Ingrese la cantidad de segundos: "))
 
 segundosTotales = calcularSegundos(dias, horas, minutos, segundos)
 
-print("La cantidad de segundos en el tiempo ingresado es de:", segundosTotales)
+print("La cantidad de segundos en el tiempo ingresado es de:", segundosTotales)"""
+
+"---------------------------------------------------------"
+
+def calcular_segundos(dias, horas, minutos, segundos):
+    return segundos + (minutos * 60) + (horas * 3600) + (dias * 86400)
+
+dias = int(input("Días: "))
+horas = int(input("Horas: "))
+minutos = int(input("Minutos: "))
+segundos = int(input("Segundos: "))
+
+total = calcular_segundos(dias, horas, minutos, segundos)
+print(f"Total de segundos: {total}") 
